932a62a2e90f6e521406cee9431e2f5a725795e0
ItsGagan <91548416+ItsGagan@users.noreply.github.com>
2023-01-07 03:32:02 +0530

    Create BestPractise10.c

---
+++ BestPractise10.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+
+int main() {
+    // Declare an integer variable
+    int x = 10;
+
+    // Use the assert macro to check that the value of x is greater than 0
+    assert(x > 0);
+
+    // Use the assert macro to check that the value of x is less than 20
+    assert(x < 20);
+
+    return 0;
+}
